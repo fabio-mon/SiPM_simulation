e87eac7bb14afa496b3cfa17ad346dc7e66458b9
Fabio Monti <fabio.monti@cern.ch>
2017-12-20 11:24:05 +0100

    useless detector construction removed

--- src/B4RunAction.cc
+++ src/B4RunAction.cc
@@ -154,37 +154,7 @@ void B4RunAction::EndOfRunAction(const G4Run* /*run*/)
   // print histogram statistics
   //
   G4AnalysisManager* analysisManager = G4AnalysisManager::Instance();
-/*  if ( analysisManager->GetH1(1) ) 
-  {
-    G4cout << G4endl << " ----> print histograms statistic ";
-    if(isMaster) {
-      G4cout << "for the entire run " << G4endl << G4endl; 
-  }
-    else {
-      G4cout << "for the local thread " << G4endl << G4endl; 
-    }
-    
-    G4cout << " EAbs : mean = " 
-       << G4BestUnit(analysisManager->GetH1(1)->mean(), "Energy") 
-       << " rms = " 
-       << G4BestUnit(analysisManager->GetH1(1)->rms(),  "Energy") << G4endl;
-    
-    G4cout << " EGap : mean = " 
-       << G4BestUnit(analysisManager->GetH1(2)->mean(), "Energy") 
-       << " rms = " 
-       << G4BestUnit(analysisManager->GetH1(2)->rms(),  "Energy") << G4endl;
-    
-    G4cout << " LAbs : mean = " 
-      << G4BestUnit(analysisManager->GetH1(3)->mean(), "Length") 
-      << " rms = " 
-      << G4BestUnit(analysisManager->GetH1(3)->rms(),  "Length") << G4endl;
 
-    G4cout << " LGap : mean = " 
-      << G4BestUnit(analysisManager->GetH1(4)->mean(), "Length") 
-      << " rms = " 
-      << G4BestUnit(analysisManager->GetH1(4)->rms(),  "Length") << G4endl;
-  }
-*/
   // save histograms & ntuple
   //
   analysisManager->Write();
